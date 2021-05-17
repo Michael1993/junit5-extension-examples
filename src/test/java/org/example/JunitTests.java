d3624075440785d41ff1369874996a72fb1ec4cd
Mihaly Verhas <Mihaly_Verhas@epam.com>
2021-05-17 16:58:36 +0200

    Standard setup

---
+++ src/test/java/org/example/JunitTests.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class JunitTests {
+
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
+
+}
